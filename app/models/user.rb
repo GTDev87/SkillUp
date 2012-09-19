7d70ba7b9285ec01e44512f8f0d6728952bcbc9a
Greg <Gregt87@gmail.com>
2012-09-19 00:45:21 -0400

    Added authentication and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
   
   field :username
+  attr_accessible :username
   validates_presence_of :username
+  validates_uniqueness_of :username
   
   field :email
+  attr_accessible :email
   validates_presence_of :email
+  validates_uniqueness_of :email
+  
+  field :password_digest
+  attr_accessible :password
+  attr_accessible :password_confirmation
+  has_secure_password
+  
   
   embeds_many :skill_inherents
   embeds_many :tasks
+  
+  def self.find_by_email(email)
+    where(:email => email).first
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,34 @@ require 'spec_helper'
 
 describe User do 
   it "should create User" do
-    user = User.new(:username => "username", :email => "user@example.com")
+    user = User.new(:username => "username", :email => "user@example.com", :password => "password", :password_confirmation => "password")
     
     user.save.should be_true
   end
   
   describe "validation" do
     it "should require that User have username" do
-      user = User.new(:email => "user@example.com")
+      user = User.new(:email => "user@example.com", :password => "password", :password_confirmation => "password")
+    
+      user.save.should be_false
+    end
+    
+    it "should require username uniqueness" do
+      User.create(:username => "username",:email => "user@example.com", :password => "password", :password_confirmation => "password")
+      user = User.new(:username => "username",:email => "otherUser@example.com", :password => "password", :password_confirmation => "password")
     
       user.save.should be_false
     end
     
     it "should require that User have email" do
-      user = User.new(:username => "username")
+      user = User.new(:username => "username", :password => "password", :password_confirmation => "password")
+    
+      user.save.should be_false
+    end
+    
+    it "should require email uniqueness" do
+      User.create(:username => "username",:email => "user@example.com", :password => "password", :password_confirmation => "password")
+      user = User.new(:username => "otherUsername",:email => "user@example.com", :password => "password", :password_confirmation => "password")
     
       user.save.should be_false
     end
@@ -23,7 +37,7 @@ describe User do
   
   describe "relations" do
     it "should give multiple skill inherents to a user" do
-      user = User.new(:username => "username", :email => "user@example.com")
+      user = User.new(:username => "username", :email => "user@example.com", :password => "password", :password_confirmation => "password")
       
       skill_inherent1 = SkillInherent.new(:skill => create(:skill))
       skill_inherent2 = SkillInherent.new(:skill => create(:skill))
@@ -37,7 +51,7 @@ describe User do
     end
     
     it "should give multiple tasks to a user" do
-      user = User.new(:username => "username", :email => "user@example.com")
+      user = User.new(:username => "username", :email => "user@example.com", :password => "password", :password_confirmation => "password")
       
       task1 = Task.new(:mission => create(:mission))
       task2 = Task.new(:mission => create(:mission))
@@ -48,6 +62,24 @@ describe User do
       user.save
       
       User.first.tasks.size.should == 2
+    end
+  end
+  
+  describe "password authentication" do
+    it "should require password and confirmation matching" do
+      user = User.new(:username => "username", :email => "user@example.com", :password => "password", :password_confirmation => "not password")
+    
+      user.save.should be_false
+    end
+  end
+  
+  describe "locating user by email" do
+    it "should find user by unique email" do
+      User.create(:username => "username", :email => "user@example.com", :password => "password", :password_confirmation => "password")
+      user = User.find_by_email("user@example.com")
+      
+      user.username.should == "username"
+      user.email.should == "user@example.com"
     end
   end
 end
